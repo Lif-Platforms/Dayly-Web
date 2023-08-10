f5a402463896f03d04bfed18f855c51b69d6adc6
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-08-09 20:53:02 -0400

    updates

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,23 +1,32 @@
 // Import Stuff
 import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import "../css/login_page/index.css";
 import { login } from "../Scripts/login_page/login"
+import Lif_logo from "../Assets/Images/Global/Lif-Logo.png";
 
 // Component for login form
 function LoginForm() {
+    // Create a useNavigate instance
+    const navigate = useNavigate();
+
     return(
-        <div className='loginForm'>
-            <div className='formContainer'>
-                <h1>Login With Lif</h1>
-                <form>
-                    <label for='userInput'>Username</label>
-                    <input placeholder='Enter your Username' id='userInput'></input>
-                    <br />
-                    <label for='userInput'>Password</label>
-                    <input placeholder='Enter your Password' id='passInput' type='password'></input>
-                    <br />
-                    <button type='button' onClick={login}>Login</button>
-                </form>
+        <div className='loginContainer'>
+            <div className='loginForm'>
+                <div className='formContainer'>
+                    <img src={Lif_logo} alt='Lif Logo' />
+                    <h1>Login With Lif</h1>
+                    <form>
+                        <label for='userInput'>Username</label>
+                        <input placeholder='Enter your Username' id='userInput'></input>
+                        <br />
+                        <label for='passInput'>Password</label>
+                        <input placeholder='Enter your Password' id='passInput' type='password'></input>
+                        <br />
+                        <button type='button' onClick={() => login(navigate)}>Login</button>
+                    </form>
+                    <span id='login_status' />
+                </div>
             </div>
         </div>
     );
--- src/Scripts/login_page/login.js
+++ src/Scripts/login_page/login.js
@@ -1,36 +1,45 @@
-export async function login() {
-    // Creates new websocket instance
-    const ws = new WebSocket('ws://localhost:8001');
+import Cookies from 'js-cookie';
 
-    // Waits for the connection to open
-    ws.addEventListener('open', () => {
-        console.log('WebSocket connection established.');
-        // Tells the server that the client is trying to log in
-        ws.send("DAYLY_LOGIN");
-    });
-
-    // Looks for incoming messages 
-    ws.addEventListener('message', (event) => {
-        var message = event.data; 
-        console.log(`Message received: ${message}`);
-        
-        // Checks if the server has asked for the credentials
-        if (message === "CREDENTIALS?") {
-            // Gets the credentials from the login form
-            const getUsername = document.getElementById('userInput');
-            const username = getUsername.value; 
-
-            const getPass = document.getElementById('passInput');
-            const password = getPass.value;
-
-            // Prepares the data to send to the server
-            const credentials = {Username:username, Password:password};
-            const data = JSON.stringify(credentials); 
-
-            // Sends data to the server
-            ws.send(data); 
+export async function login(navigate) {
+    // Get username and password from page
+    const username = document.getElementById('userInput').value; 
+    const password = document.getElementById('passInput').value; 
+    
+    // Login using auth server
+    fetch(`${process.env.REACT_APP_AUTH_URL}/login/${username}/${password}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
-      });
+        return response.json(); // Parse response as JSON
+    })
+    .then(data => {
+        // Handle the parsed data
+        console.log(data);
+
+        // Check the response of the server
+        if (data.Status === "Successful") {
+            document.getElementById("login_status").innerHTML = "Login Successful!";
+            document.getElementById("login_status").style.color = "green";
+
+            // Set cookies to keep user logged in
+            Cookies.set('LIF_USERNAME', username, {path: '/'});
+            Cookies.set('LIF_TOKEN', data.Token, {path: '/'});
+
+            // Navigate to home page
+            navigate('/');
+        } else {
+            document.getElementById("login_status").innerHTML = "Login Unsuccessful!";
+            document.getElementById("login_status").style.color = "red";
+        }
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('Fetch error:', error);
+
+        document.getElementById("login_status").innerHTML = "Something Went Wrong!";
+        document.getElementById("login_status").style.color = "red";
+    });
 }
 
 export default login;
